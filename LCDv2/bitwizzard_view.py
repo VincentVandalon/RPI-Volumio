8554e6b091ca084720ab643d7ea6a4e66d47f05d
Vincent Vandalon <vincent@vandalon.nl>
2020-04-12 08:26:26 +0000

    Got a clean I2C flow setup up for writing to the display

--- LCDv2/bitwizzard_view.py
+++ LCDv2/bitwizzard_view.py
@@ -1,3 +1,4 @@
+import time 
 import smbus
 
 class LCDDisplay(object):
@@ -39,19 +40,37 @@ class BitwizzardDisplay(LCDDisplay):
         bus.write_byte_data(self.lcdAddress,0x11,0)
         # Write data
         def writeString(s):
-            return map(lambda c:ord(c),s)
+            chars = 80*[ord(' ')]
+            i = 0
+            for c in s:
+                chars[i] = ord(c)
+                i += 1
+            return chars
 
         charText = writeString(self.displayedText)
-        l2 = charText[:19] + charText[19:38]
-        l1 = writeString("*********************") + charText[38:]
-        bus.write_i2c_block_data(self.lcdAddress, 0x00, l1[:30])
+        l1 = writeString("*******************.")[:20] + charText[20:40]
+        l2 = charText[:20] + charText[40:]
+        bus.write_i2c_block_data(self.lcdAddress, 0x00, l1[:20])
+        time.sleep(0.015)
+        bus.write_i2c_block_data(self.lcdAddress, 0x00, l1[20:40])
+        time.sleep(0.015)
 
-        bus.write_i2c_block_data(self.lcdAddress, 0x00, l2[:30])
+        bus.write_byte_data(self.lcdAddress,0x11,0x20)
+        bus.write_i2c_block_data(self.lcdAddress, 0x00, l2[:20])
+        time.sleep(0.015)
+        bus.write_i2c_block_data(self.lcdAddress, 0x00, l2[20:40])
+        time.sleep(0.015)
         bus.close()
 
 
 if __name__ == "__main__":
         # execute only if run as a script
             bw = BitwizzardDisplay()
-            bw.setText("asdfasdf")
-            bw.setText("This is a test for the display with a longer string")
+            i = 0
+            while i < 10:
+                if i%2 ==0:
+                    bw.setText("This is a test for the display with a longer string")
+                else:
+                    bw.setText("Nope")
+                time.sleep(0.5)
+                i += 1
