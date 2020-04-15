e5f79f44484fb4816416b4b404d36b00f166758f
Vincent Vandalon <vincent@vandalon.nl>
2020-04-15 06:32:41 +0200

    Got off button and toggle play/pause to work

--- testGPIO/DenonTunerControlGPIO.py
+++ testGPIO/DenonTunerControlGPIO.py
@@ -9,39 +9,37 @@ class DenonTunerGPIO:
     CLOCKWISE = 0
     ANTICLOCKWISE = 1
     
-    def __init__(self, clockPin, dataPin, switchPin):
+    def __init__(self, clockPin, dataPin, switchPin, offSwitch):
         #persist values
         self.clockPin = clockPin
         self.dataPin = dataPin
         self.switchPin = switchPin
+        self.offSwitch = offSwitch
 
         #setup pins
         GPIO.setup(clockPin,  GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(dataPin,   GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(switchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        while False:
-            sleep(0.05)
-            if GPIO.event_detected(switchPin):
-                print('Trigger:'+str(switchPin))
-            if GPIO.event_detected(clockPin):
-                print('Trigger:'+str(clockPin) + 'other:' + str(GPIO.event_detected(dataPin)))
-                #print(GPIO.input(dataPin))
-            if GPIO.event_detected(dataPin):
-                print('Trigger:'+str(dataPin) + 'other:' + str(GPIO.event_detected(clockPin)))
+        GPIO.setup(offSwitch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def start(self):
         GPIO.add_event_detect(self.switchPin,
                               GPIO.RISING,
                               callback=self._switchCallback,
                               bouncetime=100)
+        GPIO.add_event_detect(self.offSwitch,
+                              GPIO.RISING,
+                              callback=self._switchCallback,
+                              bouncetime=100)
         GPIO.add_event_detect(self.clockPin,
                               GPIO.RISING,
-                              callback=self._clockCallback,
+                              callback=self._switchCallback,
                               bouncetime=100)
 
     def stop(self):
         GPIO.remove_event_detect(self.clockPin)
         GPIO.remove_event_detect(self.switchPin)
+        GPIO.remove_event_detect(self.offSwitch)
 
     def _clockCallback(self, pin):
         #if GPIO.input(self.clockPin) == 0:
@@ -56,9 +54,14 @@ class DenonTunerGPIO:
         print(json.load(urllib.urlopen(volumioAPIString)))
     
     def _switchCallback(self, pin):
-        #if GPIO.input(self.switchPin) == 1:
-        volumioAPIString = 'http://192.168.1.105:3000/api/v1/commands/?cmd=toggle'
-        print(json.load(urllib.urlopen(volumioAPIString)))
+        if pin == self.switchPin:
+            volumioAPIString = 'http://192.168.1.105:3000/api/v1/commands/?cmd=toggle'
+            print(json.load(urllib.urlopen(volumioAPIString)))
+        if pin == self.offSwitch:
+            volumioAPIString = 'http://192.168.1.105:3000/api/v1/commands/?cmd=stop'
+            print(json.load(urllib.urlopen(volumioAPIString)))
+        if pin == self.clockPin:
+            self._clockCallback(pin)
 
 #test
 if __name__ == "__main__":
@@ -66,11 +69,12 @@ if __name__ == "__main__":
     CLOCKPIN = 6
     DATAPIN = 13
     SWITCHPIN = 5
+    OFFSWITCH = 16
 
 
     GPIO.setmode(GPIO.BCM)
     
-    denonGPIO = DenonTunerGPIO(CLOCKPIN, DATAPIN, SWITCHPIN)
+    denonGPIO = DenonTunerGPIO(CLOCKPIN, DATAPIN, SWITCHPIN, OFFSWITCH)
 
     denonGPIO.start()
 
